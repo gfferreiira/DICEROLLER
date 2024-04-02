ebe8af3ff58b3d6e4fc6674cbc81c5e57e8d5111
gfferreiira <arzhun7381@gmail.com>
2024-04-01 22:24:28 -0300

    Adicionando metodos para fazer o rolador de dados ser executado (utilizando Picker e Random)

--- DICEROLLER/MainPage.xaml.cs
+++ DICEROLLER/MainPage.xaml.cs
@@ -4,34 +4,25 @@ namespace DICEROLLER
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+            SidesPicker.SelectedIndex = 0;
         }
 
         private void BtnSpin_Clicked(object sender, EventArgs e)
         {
-            
+            int valor = 0;
+
+            //Pegar o valor que foi selecionado pelo usuario no picker
+            valor = Convert.ToInt32(SidesPicker.SelectedItem);
+            //Jogar esse valor em uma variavel
+            // sortear um numero em um dado utilizando o Random
+            // Colocar o valor de uma varíavel 
+            // Exibir o valor para o usuário na SpinCountLabel
+            var valorSorteado = 0;
+            valorSorteado = new Random().Next(1, valor+1);
+            ResultLabel.Text = valorSorteado.ToString();
         }
-            
-        void OnPickerSelectedIndexChanged(object sender, EventArgs e)
-        {
-            var picker = (Picker)sender;
-            int selectedIndex = picker.SelectedIndex;
-
-            if (selectedIndex != -1)
-            {
-                SpinCountLabel.Text = picker.Items[selectedIndex].ToString();
-               
-
-                
-            }
-            
-
-
-        }
-    
     }
 }
